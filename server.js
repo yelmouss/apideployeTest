027878f210cb279231f32a470e5d03098e92a231
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2023-09-22 12:39:10 +0100

    testing api

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+app.get('/api/users',(req,res) => {
+
+    return res.status(200).send('hello');
+})
  
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
  
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
  
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, /*hostname*/ () => {
+  console.log(`Server running at http://${port}/`);
 });
