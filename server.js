03dc5797091acba8685eec390369a170bda37db4
yelmouss <yassine.elmouss@sfr.fr>
2023-09-24 20:11:08 +0100

    cors

--- server.js
+++ server.js
@@ -33,18 +33,18 @@ app.get("/api/public", (req, res) => {
 
 
 //header d'accès global à l'API
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
 // Importer les routes d'authentification
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
