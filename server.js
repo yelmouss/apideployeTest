7a9ceff0fa55cc28e299fb189a84d0716a12877b
yelmouss <yassine.elmouss@sfr.fr>
2023-09-24 20:02:42 +0100

    save

--- server.js
+++ server.js
@@ -30,6 +30,37 @@ app.get("/api/public", (req, res) => {
     Baby: "RB",
   });
 });
+
+
+//header d'accès global à l'API
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+// Importer les routes d'authentification
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+// Importer les routes d'envoi d'email
+const MailingRoutes = require("./routes/MailingRoutes");
+app.use("/api", MailingRoutes);
+
+// Importer d'ajout de photos et articles
+const ImagesRoutes = require("./routes/Images");
+app.use("/api", ImagesRoutes);
+
+const path = require("path");
+
+// Serve the static React app (build) from the "client/build" directory
+// app.use(express.static(path.join(__dirname, "front/build")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 // Always serve the React app's HTML for all routes
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "front/build", "index.html"));
