c8b258755c5ae6f432f0267b48b99926a9f7c71d
yelmouss <yassine.elmouss@sfr.fr>
2023-09-24 18:56:08 +0100

    mongoose

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.get("/api/users", (req, res) => {
 });
 
 // Connexion à la base de données MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error(err));
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error(err));
 
 
 app.get("/api/public", (req, res) => {
