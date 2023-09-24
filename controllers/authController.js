091dc9f708fad4f2f46d8b4109ac7ca737b7745e
yelmouss <yassine.elmouss@sfr.fr>
2023-09-24 21:06:36 +0100

    bcryptjs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 // authController.js
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const nodemailer = require("nodemailer");
